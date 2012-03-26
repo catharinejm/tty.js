6a9f7f1f266d641e58352dd32d38732c7b833968
Jon Distad <jon.distad@gmail.com>
2012-03-25 23:08:37 -0400

    move basic functions outside readerFn

--- javascripts/repl.js
+++ javascripts/repl.js
@@ -5,6 +5,7 @@ function StringStream(string) {
 }
 
 StringStream.prototype.getc = function() { return this.string[this.index++]; }
+StringStream.prototype.peek = function() { return this.string[this.index]; }
 StringStream.prototype.back = function() { this.index--; }
 StringStream.prototype.chunk = function(regex) { 
   var match = (this.string.substr(this.index).match(regex || /^[^\s\(\)]*/) || [""])[0];
@@ -37,122 +38,121 @@ function Symbol(sym) {
     prompt: "REPL>"
   }
 
+  function quote(form) { 
+    form.quoted = true;
+    return form; 
+  }
+
+  function cons(car, cdr) {
+    return new Cons(car, cdr);
+  }
+
+  function car(cons) { return cons.car; }
+  function cdr(cons) { return cons.cdr; }
+
+  function type(form) {
+    return form.type || typeof(form);
+  }
+
   REPL.readerFn = function(input) { 
     try {
       var inputStream = new StringStream(input);
       var form = readForm(inputStream);
       inputStream.jump();
-      if (! inputStream.isConsumed()) throw("extraneous characters after end of input: \""+inputStream.rem()+'", '+inputStream.length+' '+inputStream.index);
+      if (! inputStream.isConsumed()) throw("extraneous characters after end of input: \""+inputStream.rem()+'"');
       return printForm(form);
     } catch(err) {
      return "READ ERROR: " + err;
     }
+  }
 
-    function readForm(input) {
-      input.jump();
-      var cur = input.getc();
-      if (cur == ')') throw('unexpected ")"');
+  function readForm(input) {
+    input.jump();
+    var cur = input.getc();
+    if (cur == ')') throw('unexpected ")"');
 
-      if (/\d/.test(cur)) {
-        input.back();
-        return readNumber(input);
-      } else if (cur == "'")
-        return quote(readForm(input));
-      else if (cur == '(')
-        return readList(input);
-      else if (cur == '"')
-        return readString(input);
-      else {
-        input.back();
-        return readSymbol(input);
-      }
-    }
-    
-    function readNumber(input) {
-      var num = input.chunk();
-      if (/\./.test(num))
-        parsedNum = parseFloat(num);
-      else
-        parsedNum = parseInt(num);
-
-      if (parsedNum != parsedNum) // NaN
-        throw(num + " is not a valid number.");
-
-      return parsedNum;
-    }
-
-    function quote(form) { 
-      form.quoted = true;
-      return form; 
-    }
-
-    function cons(car, cdr) {
-      return new Cons(car, cdr);
-    }
-
-    function car(cons) { return cons.car; }
-    function cdr(cons) { return cons.cdr; }
-
-    function type(form) {
-      return form.type || typeof(form);
-    }
-
-    function printForm(form) {
-      switch(type(form)) {
-      case "number":
-        return form.toString();
-      case "string":
-        return '"'+form+'"';
-      case "Symbol":
-        return form.sym;
-      case "Nil":
-        return "()";
-      case "Cons":
-        return "("+printList(form)+")";
-      default:
-        throw('type "'+type(form)+'" is invalid');
-      }
-    }
-
-    function printList(form) {
-      if (cdr(form) == NIL)
-        return printForm(car(form));
-      else if (type(cdr(form)) != "Cons")
-        return printForm(car(form)) + " . " + printForm(cdr(form));
-      else
-        return printForm(car(form)) + " " + printList(cdr(form));
-    }
-
-    function readList(input, idx) {
-      input.jump();
-      var cur = input.getc();
-      if (cur == ".") {
-        var form = readForm(input);
-        input.getc();
-        return form;
-      } else if (cur == ")")
-        return NIL;
-
+    if (/\d/.test(cur)) {
       input.back();
-      return cons(readForm(input), readList(input));
+      return readNumber(input);
+    } else if (cur == "'")
+      return quote(readForm(input));
+    else if (cur == '(')
+      return readList(input);
+    else if (cur == '"')
+      return readString(input);
+    else {
+      input.back();
+      return readSymbol(input);
     }
+  }
+  
+  function readNumber(input) {
+    var num = input.chunk();
+    if (/\./.test(num))
+      parsedNum = parseFloat(num);
+    else
+      parsedNum = parseInt(num);
 
-    function readString(input) {
-      var string = "", escapes;
-      do {
-        var chunk = input.chunk(/[^"]*/);
-        if (! chunk) throw("unterminated string");
-        string += chunk;
-        escapes = (string.match(/\\*$/) || [""])[0];
-      } while (escapes.length & 1);
+    if (parsedNum != parsedNum) // NaN
+      throw(num + " is not a valid number.");
+
+    return parsedNum;
+  }
+
+  function printForm(form) {
+    switch(type(form)) {
+    case "number":
+      return form.toString();
+    case "string":
+      return '"'+form+'"';
+    case "Symbol":
+      return form.sym;
+    case "Nil":
+      return "()";
+    case "Cons":
+      return "("+printList(form)+")";
+    default:
+      throw('type "'+type(form)+'" is invalid');
+    }
+  }
+
+  function printList(form) {
+    if (cdr(form) == NIL)
+      return printForm(car(form));
+    else if (type(cdr(form)) != "Cons")
+      return printForm(car(form)) + " . " + printForm(cdr(form));
+    else
+      return printForm(car(form)) + " " + printList(cdr(form));
+  }
+
+  function readList(input, idx) {
+    input.jump();
+    var cur = input.getc();
+    if (cur == ".") {
+      var form = readForm(input);
       input.getc();
-      return string;
-    }
+      return form;
+    } else if (cur == ")")
+      return NIL;
 
-    function readSymbol(input) {
-      return new Symbol(input.chunk());
-    }
-  };
+    input.back();
+    return cons(readForm(input), readList(input));
+  }
+
+  function readString(input) {
+    var string = "", escapes;
+    do {
+      var chunk = input.chunk(/[^"]*/);
+      string += chunk;
+      escapes = (string.match(/\\*$/) || [""])[0];
+    } while (escapes.length & 1);
+    if (input.getc() != '"') throw("unterminated string");
+    return string;
+  }
+
+  function readSymbol(input) {
+    return new Symbol(input.chunk());
+  }
 
   $.fn.repl = function() {
     $(this).tty(REPL.prompt, REPL.readerFn);
